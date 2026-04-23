1a899cba24786f5b4791a02bae66576b442acaa0
Mohan Gowda R <mohanrmoni54@gmail.com>
2026-04-23 16:47:58 +0530

    Add files via upload

---
+++ sample-app/src/test/java/org/bnmit/AppTest.java
@@ -0,0 +1,19 @@
+package org.bnmit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
